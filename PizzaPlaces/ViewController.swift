5a889bd398e8ba923c921636fc4b6133a5162b3f
Andrew Zborovskyi <zborowskiy@gmail.com>
2015-07-25 10:00:43 +0200

    Implemented location updating

--- PizzaPlaces/ViewController.swift
+++ PizzaPlaces/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    var locationManager: CLLocationManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.locationManager = CLLocationManager()
+        self.locationManager?.delegate = self
+        
+        let code = CLLocationManager.authorizationStatus()
+        
+        if (code == CLAuthorizationStatus.NotDetermined) {
+            self.locationManager?.requestWhenInUseAuthorization()
+        }
     }
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {}
+    
 
+    func locationManager(manager: CLLocationManager!, didUpdateToLocation newLocation: CLLocation!, fromLocation oldLocation: CLLocation!) {
+        fetchRestaurants(newLocation)
+        // One update is enough for now
+        self.locationManager?.stopUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if (status == CLAuthorizationStatus.AuthorizedWhenInUse
+            || status == CLAuthorizationStatus.AuthorizedAlways) {
+            self.locationManager?.startUpdatingLocation()
+        } else {
+            UIAlertView(title: "Please enable location access in order to use the app.", message: nil, delegate: nil, cancelButtonTitle: "OK").show()
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    
+    func fetchRestaurants(location: CLLocation) {
+        
+//        https://www.google.me/search?q=reactivecocoa&rlz=1C5CHFA_enUA505UA505&oq=reac&aqs=chrome.0.69i59j69i60j69i57j69i60j69i65j69i60.1199j0j4&sourceid=chrome&es_sm=91&ie=UTF-8
+        
+    }
+    
 }
 
--- Podfile
+++ Podfile
@@ -1,4 +1,4 @@
 source 'https://github.com/goinstant/pods-specs-public.git'
 source 'https://github.com/CocoaPods/Specs.git'
 
-pod 'MKNetworkKit'
+pod 'SDWebImage'
